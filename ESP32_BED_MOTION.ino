6837d4394620d56a77ab62d1e0591c3c97ccf534
antbern <40672068+antbern@users.noreply.github.com>
2021-10-12 22:28:24 +0200

    Add support for enabled topic and improve topic configuration
    Instead of manually changing the topic strings, they are now generated
    at compile-time using a C89 feature together with a define that contains
    the base topic.
    
    Also added support for enabling/disabling the device remotely by
    subscribing to a `/enabled` topic and improved handling of the topics
    in the MQTT callback.

--- ESP32_BED_MOTION.ino
+++ ESP32_BED_MOTION.ino
@@ -16,13 +16,19 @@ unsigned long lastMsg = 0;
 // the default interval between measurements
 unsigned long interval = 5000;
 
+volatile bool enabled = false;
 
 // the buffer used to format the JSON output to send
 #define MSG_BUFFER_SIZE  (256)
 char msg[MSG_BUFFER_SIZE];
 
-const char* topic_environment = "esp32/environment";
-const char* topic_interval = "esp32/interval";
+// the base topic to publish and listen to
+#define TOPIC_BASE "esp210"
+
+// using crude way for compile-time string concatenation using feature of the C89-compatible compiler
+const char* topic_enabled = TOPIC_BASE "/enabled";
+const char* topic_interval = TOPIC_BASE "/interval";
+const char* topic_observation = TOPIC_BASE "/observation";
 
 void setup() {
   // Start Serial
@@ -31,7 +37,6 @@ void setup() {
   // Set up pins
   pinMode(led, OUTPUT);
   digitalWrite(led, HIGH);
-
 
   start_wifi();
 
@@ -82,6 +87,7 @@ void reconnect_mqtt() {
       // do stuff when connected here (like subscribing to topics)
       
       client.subscribe(topic_interval);
+      client.subscribe(topic_enabled);
       
     } else {
       Serial.print("failed, rc=");
@@ -100,18 +106,33 @@ void callback(char* topic, byte* payload, unsigned int length) {
   for (int i = 0; i < length; i++) {
     Serial.print((char)payload[i]);
   }
+  Serial.println();
+
+  // convert topic and payload to strings
+  String topicstr(topic);
+  String payloadstr((char*)payload);
 
   // try to interpret the message as an unsigned long
-  char *end;
-  unsigned long i = strtoul((const char*)payload, &end, 10);
-
-  if (end != (char*)payload && i >= 500) {
-    // we managed to parse an unsigned long, use it as interval for now
-    Serial.print("New interval received: ");
-    Serial.println(i, DEC);
-    
-    interval = i;
+  long i = payloadstr.toInt();
+  
+  if (topicstr.endsWith("interval")) {
+    if (i >= 500) {
+      // we managed to parse an unsigned long, use it as interval for now
+      Serial.print("New interval received: ");
+      Serial.println(i, DEC);
+      
+      interval = i;
+    }
+  } else if (topicstr.endsWith("enabled")) {
+    enabled = i > 0 ? true : false;
+    Serial.print("Set enabled: ");
+    Serial.println(enabled);
   }
+}
+
+// used later for turning on/off with a physical button on the device
+void setEnabled(bool enabled) {
+  client.publish(topic_enabled, (const char*)&(enabled ? "1" : "0"), true);  
 }
 
 void loop() {
@@ -119,6 +140,11 @@ void loop() {
     reconnect_mqtt();
   }
   client.loop();
+
+  // don't continue if we are not supposed to acquire and publish anything
+  if(!enabled){
+    return;
+  }
 
   unsigned long now = millis();
   if(now - lastMsg > interval) {
@@ -138,7 +164,7 @@ void loop() {
     Serial.println(msg);
 
     // publish message
-    client.publish(topic_environment, msg);  
+    client.publish(topic_observation, msg);  
 
   }
 
